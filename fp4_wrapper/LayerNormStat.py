f6468ff90923111e9fba3ce3c0cdd9e5d369cbd1
Andrei Anufriev <andrey.anufriev@intel.com>
2023-10-06 10:07:53 +0200

    Normaliza last layer output tensor.

--- fp4_wrapper/LayerNormStat.py
+++ fp4_wrapper/LayerNormStat.py
@@ -4,15 +4,40 @@ import torch
 layer_norm_stat = {}
 
 
+# def getActivation(name):
+#     # the hook signature
+#     def hook(model, input, output):
+#         if hasattr(input, 'logits'):
+#             data = input.logit
+#         else:
+#             data = input
+#         dim = [0] if len(data[0].shape) == 2 else [0, 1]
+#         if name not in layer_norm_stat:
+#             tmp = torch.mean(input[0].detach(), dim=dim).cpu()
+#             layer_norm_stat[name] = [1, [tmp]]
+#         else:
+#             tmp = torch.mean(input[0].detach(), dim=dim).cpu()
+#             # tmp = (layer_norm_stat[name][1] * layer_norm_stat[name][0] + tmp) / (
+#             #     layer_norm_stat[name][0] + 1
+#             # )
+#             layer_norm_stat[name][0] += 1
+#             layer_norm_stat[name][1].append(tmp)
+
+#     return hook
+
 def getActivation(name):
     # the hook signature
     def hook(model, input, output):
-        dim = [0] if len(input[0].shape) == 2 else [0, 1]
+        if hasattr(input[0], 'logits'):
+            data = input[0].logits
+        else:
+            data = input[0]
+        dim = [0] if len(data.shape) == 2 else [0, 1]
         if name not in layer_norm_stat:
-            tmp = torch.mean(input[0].detach(), dim=dim).cpu()
+            tmp = torch.mean(data.detach(), dim=dim).cpu()
             layer_norm_stat[name] = [1, [tmp]]
         else:
-            tmp = torch.mean(input[0].detach(), dim=dim).cpu()
+            tmp = torch.mean(data.detach(), dim=dim).cpu()
             # tmp = (layer_norm_stat[name][1] * layer_norm_stat[name][0] + tmp) / (
             #     layer_norm_stat[name][0] + 1
             # )
@@ -21,14 +46,51 @@ def getActivation(name):
 
     return hook
 
+class Normalizer(torch.nn.Module):
+    def __init__(self, n_feaures):
+        super().__init__()
+        self.alpha = torch.nn.Parameter(torch.ones(n_feaures))
+        self.beta = torch.nn.Parameter(torch.zeros(n_feaures))
+    
+    def forward(self, x):
+        if hasattr(x, 'logits'):
+            x.logits = x.logits *  self.alpha + self.beta
+            return x
+
+        return x * self.alpha + self.beta
+
+
+class NormalizerWrapper(torch.nn.Module):
+    def __init__(self, module, n_features) -> None:
+        super().__init__()
+        self.module = module
+        device = next(module.parameters()).device
+        self.norm = Normalizer(n_features).to(device)
+
+    def forward(self , *args, **kwargs):
+        y = self.module(*args, **kwargs)
+        n_y = self.norm(y)
+        return n_y
+
+
+# def regiset_stat_hooks(hooks, layer, name=""):
+#     if isinstance(layer, torch.nn.LayerNorm):
+#         h = layer.register_forward_hook(getActivation(name))
+#         hooks.append(h)
+#         print('Hook for layer: ', name)
+#     else:
+#         for cname, clayer in layer.named_children():
+#             regiset_stat_hooks(hooks, clayer, name + "." + cname)
+
 def regiset_stat_hooks(hooks, layer, name=""):
-    if isinstance(layer, torch.nn.LayerNorm):
+    if isinstance(layer, Normalizer):
         h = layer.register_forward_hook(getActivation(name))
         hooks.append(h)
         print('Hook for layer: ', name)
     else:
         for cname, clayer in layer.named_children():
             regiset_stat_hooks(hooks, clayer, name + "." + cname)
+
 
 def ln_forward(ln, x):
     m = torch.mean(x, dim=-1, keepdim=True)
@@ -106,15 +168,55 @@ def recalibrate_normalizer(layer, fp32_stats, quant_stats, name=""):
             diff = torch.mean(torch.abs(y_q - y_fp32))
             print(diff)
         else:
-            y_fp32 = layer(x_fp32)
-            for i in range(x_q.shape[1]): # shape is [seq_len, data_dim]
-                A = x_q[:, i]
-                ones = torch.ones_like(A)
-                A = torch.stack((A, ones), dim=1).to(torch.float32)
-                B = y_fp32[:, i].unsqueeze(1).to(torch.float32)
-                alpha_beta = torch.linalg.lstsq(A, B)[0].to(x_q.dtype)
-                layer.alpha.data[i] = alpha_beta[0]
-                layer.beta.data[i] = alpha_beta[1]
+            use_pca = True
+            if use_pca:
+                y_fp32 = layer(x_fp32)
+                m = torch.mean(y_fp32, dim=0)
+                y_fp32 -= m
+
+                y_q = layer(x_q)
+                x_q -= m
+                
+                diff_before = torch.mean(torch.abs(y_fp32 - y_q))
+                pca_dim = 128
+                U, S, V = torch.torch.pca_lowrank(y_fp32, q=pca_dim)
+                y_fp32_proj = torch.matmul(y_fp32, V)
+                x_q_proj = torch.matmul(x_q, V)
+                alpha_proj = torch.ones((1, pca_dim))
+                beta_proj  = torch.zeros((1, pca_dim))
+                for i in range(x_q_proj.shape[1]): # shape is [seq_len, data_dim]
+                    A = x_q_proj[:, i]
+                    ones = torch.ones_like(A)
+                    A = torch.stack((A, ones), dim=1).to(torch.float32).to('cpu')
+                    B = y_fp32_proj[:, i].unsqueeze(1).to(torch.float32).to('cpu')
+                    alpha_beta = torch.linalg.lstsq(A, B, driver='gelss')[0].to(x_q.dtype)
+                    alpha_proj.data[0, i] = alpha_beta[0]
+                    beta_proj.data[0, i] = alpha_beta[1]
+                alpha = torch.matmul(alpha_proj.to(V.device), torch.t(V))
+                beta = torch.matmul(beta_proj.to(V.device), torch.t(V))
+
+                layer.alpha.data[:] = alpha[:]
+                layer.beta.data[:] = beta[:]
+
+                y_q = layer(x_q)
+                diff_after = torch.mean(torch.abs(y_fp32 - y_q))
+                print(diff_before, diff_after)
+            else:
+                y_fp32 = layer(x_fp32)
+                y_q = layer(x_q)
+                diff_before = torch.mean(torch.abs(y_fp32 - y_q))
+                
+                for i in range(x_q.shape[1]): # shape is [seq_len, data_dim]
+                    A = x_q[:, i]
+                    ones = torch.ones_like(A)
+                    A = torch.stack((A, ones), dim=1).to(torch.float32).to('cpu')
+                    B = y_fp32[:, i].unsqueeze(1).to(torch.float32).to('cpu')
+                    alpha_beta = torch.linalg.lstsq(A, B, driver='gelss')[0].to(x_q.dtype)
+                    layer.alpha.data[i] = alpha_beta[0]
+                    layer.beta.data[i] = alpha_beta[1]
+                y_q = layer(x_q)
+                diff_after = torch.mean(torch.abs(y_fp32 - y_q))
+                print(diff_before, diff_after)
     else:
         for cname, clayer in layer.named_children():
             recalibrate_normalizer(clayer, fp32_stats, quant_stats, name + "." + cname)
